7756c336e7ab5e95ecf32c15d79482b4eca8eea5
panfilov.v <panfilov.v@bk.ru>
2025-06-08 18:23:13 +0300

    feat(general) - favicon; auth password required

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,5 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
-    readonly VITE_ICE_SERVER:string
-    readonly VITE_ENVELOP: "development" | string
-    readonly VITE_API_ENDPOINT: string
-    readonly VITE_VK_APP_ID: number
+    readonly VITE_API_URL:string
+    readonly VITE_DIRECTION_CODE:string
 }
--- src/components/wb-form.vue
+++ src/components/wb-form.vue
@@ -2,65 +2,79 @@
   <div class="container my-5">
     <div class="row justify-content-center">
       <div class="col-lg-9">
-        <h1 class="mb-3">Место хранения</h1>
+        <h1 class="mb-3">Отписки сотрудников</h1>
         <form :class="{
           'opacity-50':isLoading
         }" @submit.prevent="onSubmit">
           <div class="row g-3">
             <div class="col-md-12">
-              <label for="location" class="form-label">ШК</label>
+              <label for="barcode" class="form-label">ШК</label>
               <div class="d-flex">
-                <input v-model="location" type="text" class="form-control" autofocus id="location" name="location" required>
-                <span @click.prevent="location = ''" class="btn btn-warning">x</span>
+                <input v-model="barcode" type="text" class="form-control" autofocus id="barcode" name="barcode" required>
+                <span @click.prevent="barcode = ''" class="btn btn-warning">x</span>
               </div>
             </div>
-            <div class="col-md-12">
-              <label for="barcode1" class="form-label">Недостача</label>
+            <div class="col-md-6">
+              <label for="shortage" class="form-label">Недостача</label>
               <div class="d-flex">
-                <input v-model="barcode1" type="text" class="form-control" id="barcode1" name="barcode[]" >
-                <span @click.prevent="barcode1 = ''" class="btn btn-warning">x</span>
+                <input v-model="shortage" type="text" inputmode="numeric" class="form-control" id="shortage" name="shortage" >
+                <span @click.prevent="shortage = ''" class="btn btn-warning">x</span>
               </div>
             </div>
-            <div class="col-md-12">
-              <label for="barcode2" class="form-label">Излишек</label>
+            <div class="col-md-6">
+              <label for="surplus" class="form-label">Излишек</label>
               <div class="d-flex">
-                <input v-model="barcode2" type="text" class="form-control" id="barcode2" name="barcode[]" >
-                <span @click.prevent="barcode2 = ''" class="btn btn-warning">x</span>
+                <input v-model="surplus" type="text" inputmode="numeric" class="form-control" id="surplus" name="surplus" >
+                <span @click.prevent="surplus = ''" class="btn btn-warning">x</span>
               </div>
             </div>
-            <div class="col-md-12">
-              <label for="barcode3" class="form-label">через ДА</label>
+            <div class="col-md-6">
+              <label for="through" class="form-label">через «Да»</label>
               <div class="d-flex">
-                <input v-model="barcode3" type="text" class="form-control" id="barcode3" name="barcode[]" >
-                <span @click.prevent="barcode3 = ''" class="btn btn-warning">x</span>
+                <input v-model="through" type="text" inputmode="numeric" class="form-control" id="through" name="through" >
+                <span @click.prevent="through = ''" class="btn btn-warning">x</span>
               </div>
             </div>
 
             <div class="col-md-6">
-              <label for="palette" class="form-label">Обезличка ШК</label>
+              <label for="depersonalization-barcode" class="form-label">Обезличка ШК</label>
               <div class="d-flex">
-                <input v-model="palette" type="text" class="form-control" id="palette" name="palette">
-                <span @click.prevent="palette = ''" class="btn btn-warning">x</span>
+                <input v-model="depersonalizationBarcode" inputmode="numeric" type="text" class="form-control" id="depersonalization-barcode" name="depersonalization-barcode">
+                <span @click.prevent="depersonalizationBarcode = ''" class="btn btn-warning">x</span>
               </div>
             </div>
             <div class="col-md-6">
-              <label for="wb-box" class="form-label">Обезличка видео</label>
+              <label for="worker" inputmode="numeric" class="form-label">ID сотрудника</label>
               <div class="d-flex">
-                <input v-model="wbBox" type="text" class="form-control" id="wb-box" name="wb-box">
+                <input v-model="worker" type="number" class="form-control" id="worker" name="worker">
+                <span @click.prevent="worker = ''" class="btn btn-warning">x</span>
               </div>
             </div>
             <div class="col-md-6">
-              <label for="count" class="form-label">ID сотрудника</label>
+              <label for="table" inputmode="numeric" class="form-label">№ Стола Приемки</label>
               <div class="d-flex">
-                <input v-model="count" type="number" class="form-control" id="count" name="count">
+                <input v-model="table" type="text" class="form-control" id="table" name="table">
+                <span @click.prevent="table = ''" class="btn btn-warning">x</span>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <label for="reason" class="form-label">Причина обезлички</label>
+              <div class="d-flex">
+                <input v-model="reason" type="number" class="form-control" id="reason" name="reason">
+                <span @click.prevent="reason = ''" class="btn btn-warning">x</span>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <label for="count" inputmode="numeric" class="form-label">Количество</label>
+              <div class="d-flex">
+                <input v-model="count" type="text" class="form-control" id="count" name="count">
                 <span @click.prevent="count = ''" class="btn btn-warning">x</span>
               </div>
             </div>
-            <div class="col-md-6">
-              <label for="product-name" class="form-label">№ Стола Приемки</label>
+            <div class="col-md-12">
+              <label for="depersonalization-video" class="form-label">Обезличка видео</label>
               <div class="d-flex">
-                <input v-model="productName" type="text" class="form-control" id="product-name" name="product-name">
-                <span @click.prevent="productName = ''" class="btn btn-warning">x</span>
+                <input v-model="depersonalizationVideo" type="text" class="form-control" id="depersonalization-video" name="depersonalization-video">
               </div>
             </div>
 
@@ -80,14 +94,16 @@
 <script lang="ts" setup>
 import {ref} from "vue";
 
-const location = ref('');
-const barcode1 = ref('');
-const barcode2 = ref('');
-const barcode3 = ref('');
-const palette = ref('');
-const wbBox = ref('');
-const count = ref('');
-const productName = ref('');
+const barcode = ref<string>();
+const shortage = ref<string>();
+const surplus = ref<string>();
+const through = ref<string>();
+const depersonalizationBarcode = ref<string>();
+const depersonalizationVideo = ref<string>();
+const worker = ref<string>();
+const table = ref<string>();
+const reason = ref<string>();
+const count = ref<string>();
 
 const isLoading = ref(false);
 
@@ -107,14 +123,7 @@ const onSubmit = async (event:any) => {
     const data = await res.json();
     if(data.success){
       alert('Успешно отправлено!')
-      location.value = '';
-      barcode1.value = '';
-      barcode2.value = '';
-      barcode3.value = '';
-      palette.value = '';
-      wbBox.value = '';
-      count.value = '';
-      productName.value = '';
+
     }
     else{
       alert('Не удалось отправить запрос');
