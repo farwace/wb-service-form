2e118c9404cff3c45a4d83267f04f24dcce99387
panfilov.v <panfilov.v@bk.ru>
2025-06-28 13:26:06 +0300

    feat(wb-form) - Отправка формы и корректное отображение ошибок от бакенда

--- src/components/wb-form.vue
+++ src/components/wb-form.vue
@@ -10,28 +10,28 @@
         }" @submit.prevent="onSubmit">
           <div class="row g-3">
             <div class="col-md-12">
-              <label for="barcode" class="form-label">ШК</label>
+              <label for="barcode" class="form-label">ШК<sup class="text-danger">*</sup></label>
               <div class="d-flex">
                 <input v-model="barcode" type="text" class="form-control" autofocus id="barcode" name="barcode" required>
                 <span @click.prevent="barcode = ''" class="btn btn-warning">x</span>
               </div>
             </div>
             <div class="col-md-6">
-              <label for="shortage" class="form-label">Недостача</label>
+              <label for="shortage" class="form-label">Недостача<sup class="text-danger">*</sup></label>
               <div class="d-flex">
                 <input v-model="shortage" type="number" inputmode="numeric" class="form-control" id="shortage" name="shortage" >
                 <span @click.prevent="shortage = ''" class="btn btn-warning">x</span>
               </div>
             </div>
             <div class="col-md-6">
-              <label for="surplus" class="form-label">Излишек</label>
+              <label for="surplus" class="form-label">Излишек<sup class="text-danger">*</sup></label>
               <div class="d-flex">
                 <input v-model="surplus" type="number" inputmode="numeric" class="form-control" id="surplus" name="surplus" >
                 <span @click.prevent="surplus = ''" class="btn btn-warning">x</span>
               </div>
             </div>
             <div class="col-md-6">
-              <label for="through" class="form-label">через «Да»</label>
+              <label for="through" class="form-label">через «Да»<sup class="text-danger">*</sup></label>
               <div class="d-flex">
                 <input v-model="through" type="number" inputmode="numeric" class="form-control" id="through" name="through" >
                 <span @click.prevent="through = ''" class="btn btn-warning">x</span>
@@ -39,42 +39,42 @@
             </div>
 
             <div class="col-md-6">
-              <label for="depersonalization-barcode" class="form-label">Обезличка ШК</label>
+              <label for="depersonalization-barcode" class="form-label">Обезличка ШК<sup class="text-danger">*</sup></label>
               <div class="d-flex">
                 <input v-model="depersonalizationBarcode" inputmode="numeric" type="number" class="form-control" id="depersonalization-barcode" name="depersonalization-barcode">
                 <span @click.prevent="depersonalizationBarcode = ''" class="btn btn-warning">x</span>
               </div>
             </div>
             <div class="col-md-6">
-              <label for="worker" class="form-label">ID сотрудника</label>
+              <label for="worker" class="form-label">ID сотрудника<sup class="text-danger">*</sup></label>
               <div class="d-flex">
                 <input v-model="worker" type="number" inputmode="numeric" class="form-control" id="worker" name="worker">
                 <span @click.prevent="worker = ''" class="btn btn-warning">x</span>
               </div>
             </div>
             <div class="col-md-6">
-              <label for="table" class="form-label">№ Стола Приемки</label>
+              <label for="table" class="form-label">№ Стола Приемки<sup class="text-danger">*</sup></label>
               <div class="d-flex">
                 <input v-model="table" type="number" inputmode="numeric"  class="form-control" id="table" name="table">
                 <span @click.prevent="table = ''" class="btn btn-warning">x</span>
               </div>
             </div>
             <div class="col-md-6">
-              <label for="reason" class="form-label">Причина обезлички</label>
+              <label for="reason" class="form-label">Причина обезлички<sup class="text-danger">*</sup></label>
               <div class="d-flex">
                 <input v-model="reason" type="text" class="form-control" id="reason" name="reason">
                 <span @click.prevent="reason = ''" class="btn btn-warning">x</span>
               </div>
             </div>
             <div class="col-md-6">
-              <label for="count" class="form-label">Количество</label>
+              <label for="count" class="form-label">Количество<sup class="text-danger">*</sup></label>
               <div class="d-flex">
                 <input v-model="count" type="number" inputmode="numeric" class="form-control" id="count" name="count">
                 <span @click.prevent="count = ''" class="btn btn-warning">x</span>
               </div>
             </div>
             <div class="col-md-12">
-              <label for="depersonalization-video" class="form-label">Обезличка видео</label>
+              <label for="depersonalization-video" class="form-label">Обезличка видео<sup class="text-danger">*</sup></label>
               <div class="d-flex flex-column">
                 <!-- Файл-инпут: сразу открывает камеру на планшете + позволяет выбрать несколько -->
                 <input
@@ -105,12 +105,31 @@
                 <div class="col-12">
                   <button :disabled="!canSubmit" type="button" @click.prevent="trySubmit" class="btn btn-dark w-100 fw-bold" >Отправить</button>
                 </div>
+                <div class="col-12 small text-danger" v-if="!canSubmit">
+                  Заполните все поля!
+                </div>
               </div>
             </div>
           </div>
         </form>
       </div>
     </div>
+
+    <Confirm
+        v-if="successText"
+        title="Успешно!"
+        :message="successText"
+        @accept="() => successText = undefined"
+        @cancel="successText = undefined;"
+    />
+
+    <Confirm
+        v-if="errorText"
+        title="Ошибка!"
+        :message="errorText"
+        @accept="() => errorText = undefined"
+        @cancel="errorText = undefined;"
+    />
 
     <Confirm
         v-if="confirmSelect"
@@ -131,12 +150,14 @@ const shortage = ref<string>();
 const surplus = ref<string>();
 const through = ref<string>();
 const depersonalizationBarcode = ref<string>();
-const depersonalizationVideo = ref<string>();
 const worker = ref<string>();
 const table = ref<string>();
 const reason = ref<string>();
 const count = ref<string>();
 const confirmSelect = ref<boolean>(false);
+
+const errorText = ref<string>();
+const successText = ref<string>();
 
 const videos = ref<File[]>([]);
 const videoPreviews = ref<string[]>([]);
@@ -165,7 +186,7 @@ const canSubmit = computed(() => {
       through.value &&
       depersonalizationBarcode.value &&
       videos.value.length > 0 &&
-      worker.value &&
+      //worker.value &&
       table.value &&
       reason.value &&
       count.value &&
@@ -182,6 +203,27 @@ const makeSubmit = () => {
   }
   confirmSelect.value = false;
 }
+
+const clearData = () => {
+  let val = undefined;
+  barcode.value = val;
+  shortage.value = val;
+  surplus.value = val;
+  through.value = val;
+  depersonalizationBarcode.value = val;
+  worker.value = val;
+  table.value = val;
+  reason.value = val;
+  count.value = val;
+
+  if(videoInput.value){
+    videoInput.value.value = '';
+  }
+  videoPreviews.value = [];
+  videos.value = [];
+}
+
+clearData();
 
 const onSubmit = async () => {
   const formData = new FormData();
@@ -203,27 +245,32 @@ const onSubmit = async () => {
 
   isLoading.value = true;
   try{
-    const res = await fetch('/add-result.php', {
+    const res = await fetch(import.meta.env.VITE_API_URL + '/api/' + import.meta.env.VITE_ENDPOINT_CODE + '/v1.0/submit/' + import.meta.env.VITE_DIRECTION_CODE, {
       method: 'POST',
-      body: formData
+      body: formData,
+      headers: {
+        'Accept': 'application/json',
+      }
     });
     isLoading.value = false;
     if(res.status != 200){
-      alert('Не удалось отправить запрос (' + res.status + ')');
+      const ddd = await res.json();
+      errorText.value = 'Не удалось отправить запрос (' + ddd?.message? ddd.message : res.status + ')';
       return;
     }
     const data = await res.json();
     if(data.success){
-      alert('Успешно отправлено!')
-
+      successText.value = 'Успешно отправлено!';
+      clearData();
     }
     else{
-      alert('Не удалось отправить запрос');
+      errorText.value = 'Не удалось отправить запрос';
+
     }
   }
   catch (e){
     isLoading.value = false;
-    alert('Ошибка при отправке запроса, попробуйте еще раз')
+    errorText.value = 'Ошибка при отправке запроса, попробуйте еще раз';
   }
 }
 
